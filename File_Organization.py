a803313ff9860392114c35b31898068e964694a2
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2024-05-13 17:15:14 +0545

    automation

---
+++ File_Organization.py
@@ -0,0 +1,11 @@
+# file handling amd file organization : navigate, rename, move , copy , remove
+
+# automate all these::
+
+import os
+print(os.getcwd())   # Current directory
+
+os.chdir('/Users/khatiwadaprajwal22icloud.com/Desktop/ML')  # change the directory 
+
+print(os.getcwd())
+print(os.getcwd())
