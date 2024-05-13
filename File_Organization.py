01e415f9c99be39f74efaa6052e48b36df5454af
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2024-05-13 18:17:15 +0545

    automation

--- File_Organization.py
+++ File_Organization.py
@@ -3,9 +3,33 @@
 # automate all these::
 
 import os
-print(os.getcwd())   # Current directory
+# print(os.getcwd())   # Current directory
 
-os.chdir('/Users/khatiwadaprajwal22icloud.com/Desktop/ML')  # change the directory 
+os.chdir('/Users/khatiwadaprajwal22icloud.com/Desktop/random')  # change the directory 
+# print(os.getcwd()) 
+# now our current directory is changed
+# Now i want to work in this directory and play with these files.
 
-print(os.getcwd())
-print(os.getcwd())
+
+# Get the list of all the files in this directory
+# for files in os.listdir():
+#     # print(files)
+
+# for files in os.listdir():
+#     if(files == '.DS_Store'):   # to avoid the DS file
+#         continue
+#     print(files)
+
+# Now
+# split the base name from the extension and modify the name and put back together:
+for files in os.listdir():
+    name,ext= os.path.splitext(files)
+    splitted = name.split("-")      #RETURNS list
+    print(splitted)
+    for s in splitted:
+        splitted = [s.strip()]
+
+    print(splitted)
+
+# now i want number in beginning , python and title and get rid of spaces
+# split this string to each and get different elements
